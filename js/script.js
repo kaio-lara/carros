f1eea792a5c70dbae3d0d495db09fbab83f16029
Kaio Affonso de Lara <131318909+kaio-lara@users.noreply.github.com>
2026-07-02 01:17:23 -0300

    1° leva de arquivos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function abrirModal(idCarro){
+    document.getElementById(idCarro).style.display = "grid";
+}
+
+function fecharModal(idCarro){
+    document.getElementById(idCarro).style.display = "none";
+}
